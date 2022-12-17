3080e0fba4fd4e478958f6f478adf8167a579368
Lee Weatherspoon <wowzame2@outlook.com>
2022-12-16 21:28:54 -0500

    Updated Directory

--- MechSyst/models.py
+++ MechSyst/models.py
@@ -7,14 +7,17 @@ from django.utils import timezone
 class RepDetails(models.Model):
     #Python has a user model where a password is built in(Like PHP admin interface and sql w/ users)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #This is the max amount of characters for the vehicle post
     Vehicle = models.CharField(max_length=600)
     repair_Details = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    check_in_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        #Checks the current time and stores as self?
+        self.check_in_date = timezone.now()
+        #saves the current self(which is the time)
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.Vehicle
